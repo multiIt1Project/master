98403d55eda6be3518aff63fe39f35a788104af4
JieunHa <148246588+hodooha@users.noreply.github.com>
2024-05-15 16:33:13 +0900

    invt에 loginDto 추가
    view 이름 page추가
    각 페이지마다 loginDto추가

--- teamProject/src/com/multi/gameProject/inventory/app/InvtRun.java
+++ teamProject/src/com/multi/gameProject/inventory/app/InvtRun.java
@@ -1,10 +1,12 @@
 package com.multi.gameProject.inventory.app;
 
-import com.multi.gameProject.inventory.view.Store;
+import com.multi.gameProject.generalUsers.model.dto.GeneralUserDto;
+import com.multi.gameProject.inventory.view.GeneralUserStorePage;
 
 public class InvtRun {
     public static void main(String[] args) {
-        new Store().storeView();
+        GeneralUserDto loginDto = new GeneralUserDto();
+        new GeneralUserStorePage(loginDto).storeView();
 //        ItemIn(vt itemInvt = new ItemInvt();
 //         new CoinInvt().coinInvtView();
 
