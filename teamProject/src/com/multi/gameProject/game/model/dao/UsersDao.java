3f3a72c91a74eefddcd6df9f766b5cfab35627b2
박재형 <168509100+ParkJaeHyung00@users.noreply.github.com>
2024-05-15 15:32:02 +0900

    Add files via upload

---
+++ teamProject/src/com/multi/gameProject/game/model/dao/UsersDao.java
@@ -0,0 +1,176 @@
+package com.multi.hdbc_h.dao;
+
+import com.multi.hdbc_h.dto.Item_invt;
+import com.multi.hdbc_h.dto.Levels;
+import com.multi.hdbc_h.dto.Record;
+import com.multi.jdbc.common.exception.MemberException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.multi.jdbc.common.JDBCTemplate.close;
+
+public class UsersDao {
+
+    private Properties prop = null;
+
+
+    public UsersDao() {
+
+
+        try {
+            prop = new Properties();
+            prop.load(new FileReader("resources/query.properties"));
+          //  prop.loadFromXML(new FileInputStream("mapper/query.xml"));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public int selectlevel(int level, Connection conn) throws MemberException {
+        ResultSet rset = null;
+        int num=0;
+        PreparedStatement ps = null;
+        String sql="SELECT * FROM LEVELS WHERE LEVEL_NO=?";
+
+
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, level);
+            rset = ps.executeQuery();
+            Levels levels=new Levels();
+            if(rset.next()){
+            levels.setScore(rset.getInt("SCORE"));
+            num=levels.getScore();
+
+            }
+        } catch (SQLException e) {
+            //  e.printStackTrace();
+            throw new MemberException("updateMember 에러 : " + e.getMessage());
+        }finally {
+            close(ps);
+        }
+
+        return num;
+    }
+
+    public int updateHIGH_SCORE(Connection conn, Record record) throws MemberException {
+        int result = 0;
+        PreparedStatement ps = null;
+
+        String sql = "UPDATE RECORD SET HIGH_SCORE = ? , LEVEL_NO = ?,TOTAL_SCORE=? WHERE USER_ID=?";
+
+        try {
+            ps = conn.prepareStatement(sql);
+
+            ps.setInt(1, record.getHigh_score());
+            ps.setInt(2, record.getLevel_no() );
+            ps.setInt(3, record.getTotal_score() );
+            ps.setString(4, record.getUser_ID() );
+
+
+
+            result = ps.executeUpdate();
+            conn.commit();
+
+        } catch (SQLException e) {
+            //    e.printStackTrace();
+
+            throw new MemberException("updateMember 에러 : " + e.getMessage());
+        }finally {
+            close(ps);
+        }
+
+        return result;
+    }
+
+    public Record selectHIGH_SCORE(String id, Connection conn) throws MemberException {
+        ResultSet rset = null;
+        int num=0;
+        PreparedStatement ps = null;
+        String sql="SELECT * FROM RECORD WHERE USER_ID=?";
+        Record record=new Record();
+
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, id);
+            rset = ps.executeQuery();
+
+            if(rset.next()){
+                record.setHigh_score(rset.getInt("HIGH_SCORE"));
+                record.setTotal_score(rset.getInt("TOTAL_SCORE"));
+                record.setUser_ID(id);
+                num=record.getHigh_score();
+
+            }
+        } catch (SQLException e) {
+            //  e.printStackTrace();
+            throw new MemberException("updateMember 에러 : " + e.getMessage());
+        }finally {
+            close(ps);
+        }
+
+        return record;
+    }
+
+    public int selectItem(String id, int item_NO,Connection conn) throws MemberException {
+        ResultSet rset = null;
+        int num=0;
+        PreparedStatement ps = null;
+        String sql="SELECT *FROM ITEM_INVT WHERE USER_ID =? AND ITEM_NO =?";
+
+
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, id);
+            ps.setInt(2,item_NO);
+            rset = ps.executeQuery();
+            Item_invt itemInvt=new Item_invt();
+            if(rset.next()){
+                itemInvt.setItem_COUNT(rset.getInt("ITEM_COUNT"));
+
+                num=itemInvt.getItem_COUNT();
+
+            }
+        } catch (SQLException e) {
+            //  e.printStackTrace();
+            throw new MemberException("updateMember 에러 : " + e.getMessage());
+        }finally {
+            close(ps);
+        }
+
+        return num;
+    }
+
+    public int updateItem(Connection conn, String id,int itemNo,int item_COUNT) throws MemberException {
+        int result = 0;
+        PreparedStatement ps = null;
+
+        String sql = "UPDATE ITEM_INVT SET ITEM_COUNT =?  WHERE USER_ID=? AND ITEM_NO =?";
+
+        try {
+            ps = conn.prepareStatement(sql);
+//
+          ps.setInt(1, item_COUNT);
+           ps.setInt(3, itemNo );
+           ps.setString(2, id );
+           conn.commit();
+            result = ps.executeUpdate();
+
+        } catch (SQLException e) {
+            //    e.printStackTrace();
+
+            throw new MemberException("updateMember 에러 : " + e.getMessage());
+        }finally {
+            close(ps);
+        }
+
+        return result;
+    }
+}
